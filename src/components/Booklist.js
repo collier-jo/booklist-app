992439e8f66daf6c0b1cf3c3ca1ceaea34320961
joannacollier <collj035@gmail.com>
2020-08-31 17:13:10 +0100

    Booklist folder

---
+++ src/components/Booklist.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Booklist = (props) => {
+    return (  
+        <div className="book-list">
+            <ul>
+                <li>The Palm-Wine Drinkard</li>
+                <li>The Left Hand of Darkness</li>
+                <li>Salt Slow</li>
+            </ul>
+        </div>
+    );
+}
+ 
+export default Booklist;
